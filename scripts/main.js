d7b38c398a39f847a6e862fbf6420faa1a94a094
tosmel2 <adewaletosin0808@gmail.com>
2023-07-14 15:48:57 -0700

    feat: connect wallet button js code integrated

--- scripts/main.js
+++ scripts/main.js
@@ -68,6 +68,71 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
     console.error('Error fetching products:', error);
   });
 
+
+
+// =====================connect wallet button===================
+const walletButton = document.querySelectorAll(".connect-wallet-btn");
+// const addressOnCard = document.getElementById("walletAddress");
+
+const updateUI = async () => {
+  if (window.ethereum !== undefined) {
+    const userWalletAddress = await window.ethereum.request({ method: "eth_accounts" });
+    console.log(userWalletAddress);
+    
+    // const slicedAddress = `${userWalletAddress[0].slice(0, 6)}.....${userWalletAddress[0].slice(userWalletAddress[0].length - 10)}`; 
+    // addressOnCard.innerHTML = slicedAddress;
+  }
+
+  walletButton.forEach((btn) => {
+    btn.innerHTML = "Wallet Connected";
+  });
+};
+
+async function handleConnectWallet() {
+  if (window.ethereum !== undefined) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  } else {
+    alert("Oops!!! Please install the MetaMask browser extension to proceed with sending Ethereum on Deeco");
+  }
+  
+  window.localStorage.setItem("connected", "true");
+  updateUI();
+}
+
+const updateConnectionState = () => {
+  if (window.localStorage.getItem("connected")) {
+    updateUI();
+  }
+};
+
+walletButton.forEach((btn) => {
+  btn.addEventListener("click", handleConnectWallet);
+});
+
+window.addEventListener("DOMContentLoaded", updateConnectionState);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //   // Get the top-nav-container element
 // const topNavContainer = document.querySelector('.top-nav-container');
 
